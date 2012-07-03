06d3680903cb103c9fcec0356e791a427d586678
Rimenes Ribeiro <eu@rimenes.net>
2012-07-02 22:30:04 -0300

    Remove the open graph redirect

---
+++ facebook.com.js
@@ -0,0 +1,45 @@
+// open graph redirect
+// code from: https://raw.github.com/jodoglevy/open-graph-redirect/master/chrome/redirect.js
+
+function getURLParameterByName(url, name) {
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec(url);
+
+  if(results == null) return null;
+  else return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+function replaceOpenGraphTags() {
+	var links, numLinks, i, redirectURI;
+
+	links = document.querySelectorAll('a[href*="connect/uiserver.php"]');
+	numLinks = links.length;
+
+	for (i = 0; i < numLinks; i++) {
+		redirectURI = getURLParameterByName( links[i].getAttribute('href'), "redirect_uri");
+		if (redirectURI) {
+            links[i].setAttribute('href', redirectURI);
+            links[i].removeAttribute('rel');
+            links[i].setAttribute('target', '_blank');
+        }
+	}
+}
+
+function checkForOpenGraphOAuth() {
+    if(document.URL.indexOf("dialog/oauth") !== -1) {
+        var redirectURI = getURLParameterByName( document.URL, "redirect_uri");
+        var scope = getURLParameterByName( document.URL, "scope");
+
+        if(redirectURI && scope && scope.indexOf("publish_actions") !== -1) {        
+            window.location = redirectURI;
+        }
+    }
+}
+
+
+checkForOpenGraphOAuth();
+replaceOpenGraphTags();
+document.addEventListener('DOMSubtreeModified', replaceOpenGraphTags, false);
